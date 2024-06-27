f7cb0ab1cbc612d0deb78b430cbd203c80617744
Spenbert02 <spenbert02@gmail.com>
2024-06-27 16:01:10 -0500

    c structure testing

--- myomappy/rungekutta/__init__.py
+++ myomappy/rungekutta/__init__.py
@@ -1,1 +1,2 @@
-from .runge_kutta_4 import rk4Track
+from . import runge_kutta_4
+from .runge_kutta_4 import *
--- myomappy/rungekutta/runge_kutta_4.py
+++ myomappy/rungekutta/runge_kutta_4.py
@@ -2,8 +2,8 @@
 4th order Runge Kutta Tracking
 """
 
-import runge_kutta_4_calcs
+from myomappy.rungekutta import _runge_kutta_4_calcs
 
 def rk4Track():
     print("4th order runge kutta track")
-    print(runge_kutta_4_calcs.foo())
+    print(_runge_kutta_4_calcs.foo())
